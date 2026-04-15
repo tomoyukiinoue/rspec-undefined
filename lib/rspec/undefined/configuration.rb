402c6b311ada83fc8a880c02abafb0e8143be691
Tomoyuki INOUE <tomoyuki.inoue@gmail.com>
2026-04-15 20:03:39 +0900

    refactor: Configuration をスレッドセーフ化

--- lib/rspec/undefined/configuration.rb
+++ lib/rspec/undefined/configuration.rb
@@ -6,32 +6,44 @@ module RSpec
       ALLOWED_FORMATS = [:json, :yaml, :csv, :markdown].freeze
       TRUE_VALUES = %w[1 true yes].freeze
 
-      attr_accessor :report_path
-      attr_reader :report_format
-
       def initialize(env: ENV)
         @env = env
         @strict_explicit = nil
         @report_path = nil
         @report_format = :json
+        @mutex = Mutex.new
       end
 
       def strict?
-        return @strict_explicit unless @strict_explicit.nil?
-        v = @env["RSPEC_UNDEFINED_STRICT"]
-        return false if v.nil?
-        TRUE_VALUES.include?(v.to_s.downcase)
+        @mutex.synchronize do
+          return @strict_explicit unless @strict_explicit.nil?
+          v = @env["RSPEC_UNDEFINED_STRICT"]
+          return false if v.nil?
+          TRUE_VALUES.include?(v.downcase)
+        end
       end
 
       def strict=(value)
-        @strict_explicit = value ? true : false
+        @mutex.synchronize { @strict_explicit = value ? true : false }
       end
 
       def report_format=(value)
         unless ALLOWED_FORMATS.include?(value)
           raise ArgumentError, "report_format must be one of #{ALLOWED_FORMATS.inspect}"
         end
-        @report_format = value
+        @mutex.synchronize { @report_format = value }
+      end
+
+      def report_format
+        @mutex.synchronize { @report_format }
+      end
+
+      def report_path
+        @mutex.synchronize { @report_path }
+      end
+
+      def report_path=(value)
+        @mutex.synchronize { @report_path = value }
       end
 
       def register_categories(*names)
